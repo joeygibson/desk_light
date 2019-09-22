5783d1c30b65a6f48f099112d41d6ffa0a37e947
Joey Gibson <joey@joeygibson.com>
2019-09-22 16:31:56 -0400

    initial commit

---
+++ code.py
@@ -0,0 +1,106 @@
+import time
+
+import board
+import neopixel
+from digitalio import DigitalInOut, Pull
+
+from number_generator import NumberGenerator
+
+MIN_BRIGHTNESS = 0.1
+MAX_BRIGHTNESS = 1.0
+
+num_pixels = 10
+pixels = neopixel.NeoPixel(board.NEOPIXEL, num_pixels, brightness=MIN_BRIGHTNESS, auto_write=False)
+
+button_a = DigitalInOut(board.BUTTON_A)
+button_a.switch_to_input(pull=Pull.DOWN)
+
+button_b = DigitalInOut(board.BUTTON_B)
+button_b.switch_to_input(pull=Pull.DOWN)
+
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        return 0, 0, 0
+    if pos < 85:
+        return 255 - pos * 3, pos * 3, 0
+    if pos < 170:
+        pos -= 85
+        return 0, 255 - pos * 3, pos * 3
+    pos -= 170
+    return pos * 3, 0, 255 - pos * 3
+
+
+def color_chase(color, wait):
+    for i in range(num_pixels):
+        pixels[i] = color
+        time.sleep(wait)
+        pixels.show()
+    time.sleep(0.5)
+
+
+def brightness_down(brightness_value):
+    if brightness_value > MIN_BRIGHTNESS:
+        brightness_value -= 0.1
+
+    return brightness_value
+
+
+def brightness_up(brightness_value):
+    if brightness_value < MAX_BRIGHTNESS:
+        brightness_value += 0.1
+
+    return brightness_value
+
+
+def rainbow_cycle(wait):
+    brightness = MIN_BRIGHTNESS
+    ng = NumberGenerator(0, 256)
+
+    for j in iter(ng):
+        initial_a = button_a.value
+        initial_b = button_b.value
+
+        for i in range(num_pixels):
+            rc_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(rc_index & 255)
+        pixels.show()
+
+        if not button_a.value and initial_a:
+            brightness = brightness_down(brightness)
+        if not button_b.value and initial_b:
+            brightness = brightness_up(brightness)
+
+        pixels.brightness = brightness
+        time.sleep(wait)
+
+
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+
+while True:
+    # pixels.fill(RED)
+    # pixels.show()
+    # # Increase or decrease to change the speed of the solid color change.
+    # time.sleep(1)
+    # pixels.fill(GREEN)
+    # pixels.show()
+    # time.sleep(1)
+    # pixels.fill(BLUE)
+    # pixels.show()
+    # time.sleep(1)
+    #
+    # color_chase(RED, 0.1)  # Increase the number to slow down the color chase
+    # color_chase(YELLOW, 0.1)
+    # color_chase(GREEN, 0.1)
+    # color_chase(CYAN, 0.1)
+    # color_chase(BLUE, 0.1)
+    # color_chase(PURPLE, 0.1)
+
+    rainbow_cycle(0)  # Increase the number to slow down the rainbow
