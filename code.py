a096aa1f798f383a8166be05b8bc8f26336ddb91
Joey Gibson <joey@joeygibson.com>
2019-09-22 16:34:33 -0400

    removed obsolete code

--- code.py
+++ code.py
@@ -33,14 +33,6 @@ def wheel(pos):
     return pos * 3, 0, 255 - pos * 3
 
 
-def color_chase(color, wait):
-    for i in range(num_pixels):
-        pixels[i] = color
-        time.sleep(wait)
-        pixels.show()
-    time.sleep(0.5)
-
-
 def brightness_down(brightness_value):
     if brightness_value > MIN_BRIGHTNESS:
         brightness_value -= 0.1
@@ -77,30 +69,5 @@ def rainbow_cycle(wait):
         time.sleep(wait)
 
 
-RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
-GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-PURPLE = (180, 0, 255)
-
 while True:
-    # pixels.fill(RED)
-    # pixels.show()
-    # # Increase or decrease to change the speed of the solid color change.
-    # time.sleep(1)
-    # pixels.fill(GREEN)
-    # pixels.show()
-    # time.sleep(1)
-    # pixels.fill(BLUE)
-    # pixels.show()
-    # time.sleep(1)
-    #
-    # color_chase(RED, 0.1)  # Increase the number to slow down the color chase
-    # color_chase(YELLOW, 0.1)
-    # color_chase(GREEN, 0.1)
-    # color_chase(CYAN, 0.1)
-    # color_chase(BLUE, 0.1)
-    # color_chase(PURPLE, 0.1)
-
     rainbow_cycle(0)  # Increase the number to slow down the rainbow
